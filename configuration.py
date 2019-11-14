25f20039b6ced8c60be8c38aa42306eaf41aea58
txoof <aaron.ciuffo@gmail.com>
2019-11-14 22:36:01 +0100

    add epd update to now-playing loop in main()

--- configuration.py
+++
@@ -1,6 +0,0 @@
-CONFIGFILE = './resources/config.ini'
-LOGCONFIG = './resources/logging.cfg'
-
-CONFIGDEFAULTS = {'server':  {'host': None, 'port': 9000, 'player': None},
-                  'screen':  {'epdtype': None},
-                  'display': {'artistsize': 24, 'tracksize': 36},
