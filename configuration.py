fe9d8aa17c1311158ad13e0bccf78a25cfdca255
txoof <aaron.ciuffo@gmail.com>
2019-11-14 18:10:46 +0100

    add configuration file

---
+++ configuration.py
@@ -0,0 +1,6 @@
+CONFIGFILE = './resources/config.ini'
+LOGCONFIG = './resources/logging.cfg'
+
+CONFIGDEFAULTS = {'server':  {'host': None, 'port': 9000, 'player': None},
+                  'screen':  {'epdtype': None},
+                  'display': {'artistsize': 24, 'tracksize': 36},
