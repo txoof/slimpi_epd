107b116c9db5f44df4fe0a30bfced52cf4d90594
txoof <aaron.ciuffo@gmail.com>
2019-11-30 00:36:42 +0100

    remove font constant

--- epdlib/constants.py
+++ epdlib/constants.py
@@ -2,9 +2,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from pathlib import Path
+#from pathlib import Path
 
-FONT = str(Path('./fonts/Open_Sans/OpenSans-Regular.ttf').resolve())
+#FONT = str(Path('./fonts/Open_Sans/OpenSans-Regular.ttf').resolve())
 
 # US English - Upper and lower case letters
 USA_CHARDIST = {
