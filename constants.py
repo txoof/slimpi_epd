9bea083f15cda2a5a99b655759ebeca15a1b1f2d
txoof <aaron.ciuffo@gmail.com>
2019-11-30 22:56:24 +0100

    reformat

--- constants.py
+++ constants.py
@@ -1,6 +1,15 @@
+# application version number
+version = 'version 0.2.0'
+# url of github project
+url = 'https://github.com/txoof/slimpi_epd'
+# short name of application
 app_name = 'slimpi'
+# developer's name
 devel_name = 'com.txoof'
-waveshare = 'waveshare_epd.' # note trailing '.' !!
+# EPD library
+waveshare = 'waveshare_epd' 
+# file with layouts
 layouts = 'layouts'
+# location of default image for albums that fail to return ablum art
 noartwork = './images/No-album-art.png'
-version = '0.2.0'
+
