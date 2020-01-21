3125b5bce8cb79ef473b3ced71b72554bb9aa27f
txoof <aaron.ciuffo@gmail.com>
2020-01-21 22:05:32 +0100

    preparing for release

--- build.sh
+++ build.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
+
+reqPackages=("libtiff5-dev" "libopenjp*-dev")
+
+for i in "${reqPackages[@]}"
+do
+  echo checking package $i
+  if [ $(dpkg-query -W -f='${Status}' $i 2>/dev/null | grep -c "ok installed") -eq 0 ];
+  then
+    echo package: $i not installed. Install with:
+    echo $ sudo apt-get install $i
+    echo ""
+  else
+    echo $i...ok
+    echo ""
+  fi
+done
+
+
 pipenvExec=`which pipenv`
 
-# check if libopen and libtiff are installed
-# apt -qq list libopenjp2-7-dev
-# apt -qq list `libbtiff5-dev
+echo "checking pipenv virtual environment"
 
 if [[ -x ${pipenvExec} ]]; then
-  echo "pipenv --three; pipenv sync"
-  #pipenv --three
-  #pipenv sync
+  # check if there's a virutal environment
+  pipenv --venv 2>/dev/null
+  if [[ $? -ne 0 ]]; then
+    echo "building pipenv virtual environment"
+    pipenv --three
+    pipenv sync
+  else
+    echo "sycning packages"
+    echo pipenv sync
+    pipenv sync
+  fi
 else
   echo "building requires pipenv use:
 $ sudo pip3 install pipenv"
@@ -16,5 +40,5 @@ $ sudo pip3 install pipenv"
 fi
 
 echo "building slimpi media player"
-#pipenv run pyinstaller --noconfirm slimpi.spec
+pipenv run pyinstaller --clean --noconfirm slimpi.spec
 
