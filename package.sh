6dd15c514d8860078713ee9415fb6495d13293df
txoof <aaron.ciuffo@gmail.com>
2020-10-02 21:45:05 +0100

    fix regex

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 appName="slimpi"
 
-version=`cat constants.py | sed -ne 's/version\W\{0,\}=\W\{0,\}\(.*\)["'"'"']/\1/p'`
+version=`cat constants.py | sed -ne 's/^version\W\{0,\}=\W\{0,\}\(.*\)["'"'"']/\1/p'`
 echo version number is $version
 filename=$appName\_$version.tgz
 latestName=$appName\_latest.tgz
